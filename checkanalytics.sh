e2371d942e7a2e8b2bb12678fdbde9acad4c29f0
Hampus Brynolf <hampus.brynolf@gmail.com>
2013-11-25 20:56:02 +0100

    Create checkanalytics.sh

---
+++ checkanalytics.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+IFS=$'\r\n' XYZ=($(cat $1))
+
+for var in "${XYZ[@]}"
+do
+	printf ${var}
+	
+	resp=$(curl -sL $var)
+	if [[ "$resp" =~ _gaq|google-analytics ]] ; then 
+		printf "\t\033[31mANALYTICS\033[39m "
+	elif [[ "$resp" =~ ga.js|Analytics|analytics ]]; then
+		printf "\t\033[31mGA.JS\033[39m "	
+	elif [[ "$resp" =~ webtrends|WebTrends ]]; then
+		printf "\t\033[31mWEBTRENDS\033[39m "	
+	elif [[ "$resp" =~ piwikTracker ]]; then
+		printf "\t\033[31mPIWIK\033[39m "	
+		
+	else
+		printf "\t\033[32mNO\033[39m"	
+	fi
+	echo 
+done
